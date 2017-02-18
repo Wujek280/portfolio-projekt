3e0c418bd779667fb31a05d016dbc045478859a2
Wujek <mjwojcik94@gmail.com>
2017-02-18 11:00:50 +0100

    class method

--- js/header.js
+++ js/header.js
@@ -1,17 +1,23 @@
      'use strict';
 
+
 $(function(){
+   
+   var output = $(window).scrollTop();
+   console.log(output);
+   
+$("#bar").addClass("scrool");
 
    $(document).scroll(function() { 
       if($(window).scrollTop() !== 0) {
-         console.log("hedaer.js >> top");
-         
-         $("#bar").addClass("rest");
-  
-      }else{
          console.log("hedaer.js >> rest");
          
-         $("#bar").removeClass("rest");
+         $("#bar").addClass("scrool");
+  
+      }else{
+         console.log("hedaer.js >> top");
+         
+         $("#bar").removeClass("scrool");
 
       }
    });
